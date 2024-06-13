23257d9fce564d590fd25c23b01d30df725ea474
Victor Medellin <arturomdlln@gmail.com>
2024-06-13 17:58:24 -0600

    Se añadieron las rutas

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express= require("express")
+const morgan = require("morgan")
 const app = express()
 const port = 3500
+const comidaRouter = require('./routes/comidaRouter')
 
-app.get("/", (req,res)=>{
-    res.json({mensaje: "Aplicacion funcionando correctamente"})
-})
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
+app.use(morgan('dev'))
+
+app.use('/API', comidaRouter)
+
+app.use((req,res)=> {
+    res.status(404).json({ message: 'Ruta no encontrada'});
+});
 
 app.listen(port,()=>{
-    console.log(`Aplicaion corriendo en el purto ${port}`)
+    console.log(`Aplicacion corriendo en el puerto ${port}`)
 })
